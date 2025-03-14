4a4e3f8c7c07d66eb6203fbbe243830b8d552131
Lui Zappitelli <luizappitelli@gmail.com>
2025-03-13 20:13:00 -0500

    OfflineNotice created

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -7,10 +7,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
         <AppNavigator />
       </NavigationContainer>
